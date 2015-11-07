StephenOrJames/ElevatorSimulator

commit 2df1d5e1c6836445164c1c80bb3f891e68df555c

Update Elevator's current floor before the elevator stops

elevator.py
@@ -22,8 +22,8 @@ class Elevator(object):
 
 	def do(self, seconds, action):
 		"""Waits for a period of seconds to simulate an action being carried out"""
-		print("%s (%d seconds)" % (action, seconds))
-		print(self.get_status())
+		#print("%s (%d seconds)" % (action, seconds))
+		#print(self.get_status())
 		time.sleep(seconds)
 
 	def get_status(self):
@@ -64,18 +64,18 @@ class Elevator(object):
 				if (self.current_floor == original_floor):
 					self.do(1, "Starting elevator")
 				self.do(2, "Moving from floor %d to %d" % (self.current_floor, self.current_floor + 1))
+				self.current_floor += 1
 				if (self.current_floor == target_floor - 1):
 					self.do(3, "Stopping elevator")
-				self.current_floor += 1
 			# Going down!
 			elif (self.current_floor > target_floor):
 				self.going_up = False
 				if (self.current_floor == original_floor):
 					self.do(1, "Starting elevator")
 				self.do(2, "Moving from floor %d to %d" % (self.current_floor, self.current_floor - 1))
+				self.current_floor -= 1
 				if (self.current_floor == target_floor + 1):
 					self.do(3, "Stopping elevator")
-				self.current_floor -= 1
 
 	def next_floor_up(self):
 		"""Returns the next floor above the current floor to go to or None"""

commit 371f1bab100710a41d97c30280f5f8f0abe9424c

fixed bug caused by 2df1d5e

elevator.py
@@ -65,7 +65,7 @@ class Elevator(object):
 					self.do(1, "Starting elevator")
 				self.do(2, "Moving from floor %d to %d" % (self.current_floor, self.current_floor + 1))
 				self.current_floor += 1
-				if (self.current_floor == target_floor - 1):
+				if (self.current_floor == target_floor):
 					self.do(3, "Stopping elevator")
 			# Going down!
 			elif (self.current_floor > target_floor):
@@ -74,7 +74,7 @@ class Elevator(object):
 					self.do(1, "Starting elevator")
 				self.do(2, "Moving from floor %d to %d" % (self.current_floor, self.current_floor - 1))
 				self.current_floor -= 1
-				if (self.current_floor == target_floor + 1):
+				if (self.current_floor == target_floor):
 					self.do(3, "Stopping elevator")
 
 	def next_floor_up(self):